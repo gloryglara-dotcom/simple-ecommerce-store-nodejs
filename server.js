c4483e27dc82a6d6a1f497b77149959a5634f27f
gloryglara-dotcom <gloryglara@gmail.com>
2025-12-17 11:39:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,21 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+let users = [];
 let products = [
   { id: 1, name: "Mobile", price: 10000 },
   { id: 2, name: "Laptop", price: 40000 }
 ];
-
 let cart = [];
+
+app.post("/register", (req, res) => {
+  users.push(req.body);
+  res.json({ message: "User registered" });
+});
+
+app.post("/login", (req, res) => {
+  res.json({ message: "Login successful" });
+});
 
 app.get("/products", (req, res) => {
   res.json(products);
@@ -20,9 +29,10 @@ app.post("/cart", (req, res) => {
 
 app.post("/order", (req, res) => {
   cart = [];
-  res.json({ message: "Order placed successfully" });
+  res.json({ message: "Order placed" });
 });
 
-app.listen(3000, () => {
-  console.log("E-commerce server running on port 3000");
-});
+app.listen(3000, () =>
+  console.log("E-commerce app running on port 3000")
+);
+
